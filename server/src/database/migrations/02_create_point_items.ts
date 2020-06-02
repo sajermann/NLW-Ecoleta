506eed9059d19667effed59c52564b3018e1523d
Bruno Sajermann <contato@sajermann.com>
2020-06-02 17:45:25 -0300

    Day 2 Complete

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,27 @@
+import Knex from 'knex';
+/*
+export async function up(){
+}
+export async function down(){
+}*/
+
+export async function up(knex: Knex){
+  //criar a tabela
+  return knex.schema.createTable('point_items', table => {
+    table.increments('id').primary();
+    table.integer('point_id')
+      .notNullable()
+      .references('id')
+      .inTable('points');
+
+    table.integer('item_id')
+      .notNullable()
+      .references('id')
+      .inTable('items');
+  });
+}
+
+export async function down(knex: Knex){
+  // Voltar atrás
+  return knex.schema.dropTable('point_items');
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuário');
-  response.json([
-    'Bruno',
-    'Sajermann',
-    'Marcia'
-  ])
-});
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
 
 app.listen(3333);
